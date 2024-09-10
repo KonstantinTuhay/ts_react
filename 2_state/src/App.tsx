3c45e31f2d215573130d01841ffadfa16a0c8de9
Konstantin Tuhay <tugay-k@mail.ru>
2024-09-10 13:38:28 +0300

    add: type states

--- 2_state/src/App.tsx
+++ 2_state/src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { JSX, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = (): JSX.Element => {
+  const [text1, setText1] = useState<string[]>([]);
+  const [text2, setText2] = useState([""]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  type TaskType = {
+    id: number;
+    title: string;
+    isompleted: boolean;
+  };
 
-export default App
+  const [task, setTask] = useState<TaskType | null>(null);
+
+  return <>dsfdsfds</>;
+};
+
+export default App;
